72c92810f3dc8666c950dff180e6cba661a49b67
Parthsoni1 <48277567+Parthsoni1@users.noreply.github.com>
2019-09-23 17:55:52 +0530

    Add files via upload

---
+++ src/Components/Person.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Person.css'
+const Person = (props) => {
+    return (
+        <div className="Person">
+        <p onClick= {props.click} >i'm {props.name} and {props.age}  old</p>
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}/>
+        </div>
+    )
+}
+
+
+export default Person;
+
